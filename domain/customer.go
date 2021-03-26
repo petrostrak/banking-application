84e9e76d7b7f127d1a79525bbe779b0b84e604ae
petrostrak <pit.trak@gmail.com>
2021-03-26 11:06:20 +0200

    Data Transfer Object

--- domain/customer.go
+++ domain/customer.go
@@ -1,6 +1,9 @@
 package domain
 
-import "petrostrak/banking-application/errs"
+import (
+	"petrostrak/banking-application/dto"
+	"petrostrak/banking-application/errs"
+)
 
 type Customer struct {
 	ID          string `db:"customer_id"`
@@ -9,6 +12,25 @@ type Customer struct {
 	Zipcode     string
 	DataOfBirth string `db:"date_of_birth"`
 	Status      string
+}
+
+func (c Customer) statusAsText() string {
+	statusAsText := "active"
+	if c.Status == "0" {
+		statusAsText = "inactive"
+	}
+	return statusAsText
+}
+
+func (c Customer) ToDto() dto.CustomerResponse {
+	return dto.CustomerResponse{
+		ID:          c.ID,
+		Name:        c.Name,
+		City:        c.City,
+		Zipcode:     c.Zipcode,
+		DataOfBirth: c.DataOfBirth,
+		Status:      c.statusAsText(),
+	}
 }
 
 type CustomerReposiroty interface {
